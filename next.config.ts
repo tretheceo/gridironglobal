c88a7a01d5dd38918de07ec99e62f594a99f0017
tretheceo <thedantretaylor@gmail.com>
2025-05-16 19:58:18 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }
+};
+
+export default nextConfig;
