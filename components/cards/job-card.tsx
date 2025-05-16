97400022f2da17a8a9115711721ebf56a77798fd
tretheceo <thedantretaylor@gmail.com>
2025-05-16 23:00:41 +0200

    Update job-card.tsx

--- components/cards/job-card.tsx
+++ components/cards/job-card.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
